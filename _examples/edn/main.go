76acfe08d6ac035d18df97e2328a49fa6fa22908
zhangcar <zhangcar@ms.com>
2025-07-16 09:25:00 +0000

    Add example for parsing EDN files

---
+++ _examples/edn/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"github.com/alecthomas/kong"
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/alecthomas/repr"
+	"os"
+)
+
+type Boolean bool
+
+func (b *Boolean) Capture(values []string) error {
+	switch values[0] {
+	case "true":
+		*b = true
+	default:
+		*b = false
+	}
+
+	return nil
+}
+
+type Nil bool
+
+func (n *Nil) Capture(values []string) error {
+	switch values[0] {
+	case "nil":
+		*n = true
+	default:
+		*n = false
+	}
+
+	return nil
+}
+
+type EDN struct {
+	Pos   lexer.Position
+	Forms []Form `parser:"@@*"`
+}
+
+type Form struct {
+	Pos     lexer.Position
+	Symbol  *string    `parser:"  @Symbol"`
+	Keyword *string    `parser:"| @Keyword"`
+	Integer *int64     `parser:"| @Integer"`
+	Float   *float64   `parser:"| @Float"`
+	List    []Form     `parser:"| '(' @@* ')'"`
+	Vector  []Form     `parser:"| '[' @@* ']'"`
+	Set     []Form     `parser:"| '#{' @@* '}'"`
+	Map     []MapEntry `parser:"| '{' @@* '}'"`
+	String  *string    `parser:"| @String"`
+	Nil     Nil        `parser:"| @Nil"`
+}
+
+type MapEntry struct {
+	Pos   lexer.Position
+	Key   Form `parser:"@@"`
+	Value Form `parser:"@@"`
+}
+
+var (
+	ednLexer = lexer.MustSimple([]lexer.SimpleRule{
+		{Name: "Float", Pattern: `[+-]?[0-9]*\.[0-9]+`}, // 64-bit
+		{Name: "Integer", Pattern: `[+-]?[0-9]+`},       // 64-bit
+		{Name: "Keyword", Pattern: `:[#.*+!_?$%&=<-]?[a-zA-Z0-9][a-zA-Z0-9#.*+!_?$%&=<-]*/?[a-zA-Z0-9#.*+!_?$%&=<-]*`},
+		{Name: "String", Pattern: `"(\\"|[^"])*"`},
+		{Name: "Character", Pattern: `\b(\\\w|\\u\d{Name: 4})\b`},
+		{Name: "Symbol", Pattern: `[.+-]?[a-zA-Z][a-zA-Z0-9:#.*+!_?$%&=<-]*/?[a-zA-Z0-9:#.*+!_?$%&=<-]+`},
+		{Name: "Nil", Pattern: `\bnil\b`},
+		{Name: "comment", Pattern: `;[^\n]*|#_(#?\{[^}]*}|\[[^]]*\]|\([^\)]*\)|[^\s]*)`}, // comment
+		{Name: "special", Pattern: `#?[][}{)(]`},
+		{Name: "whitespace", Pattern: `[,\s]+`},
+})
+
+	ednParser = participle.MustBuild[EDN](
+		participle.Lexer(ednLexer),
+		participle.Unquote("String"),
+		participle.UseLookahead(2),
+	)
+
+	cli struct {
+		File string `help:"EDN file to parse." arg:""`
+	}
+)
+
+func main() {
+	ctx := kong.Parse(&cli)
+	var r *os.File
+	var err error
+	r, err = os.Open(cli.File)
+	ctx.FatalIfErrorf(err)
+	defer r.Close()
+	edn, err := ednParser.Parse(cli.File, r)
+	ctx.FatalIfErrorf(err)
+	repr.Println(edn)
+}
